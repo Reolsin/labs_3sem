735fffd08ce21988449fb6ae24d267c73843935c
Reolsin <71131979+Reolsin@users.noreply.github.com>
2020-09-25 12:48:39 +0300

    Update Astroid.cpp

--- Astroid.cpp
+++ Astroid.cpp
@@ -21,7 +21,7 @@ namespace Prog2 {
 		R = r;
 	}
 
-	Astroid::Astroid(double x0, double y0, double r) : C(x0,y0)
+	Astroid::Astroid(double x0, double y0, double r) : C(x0, y0)
 	{
 		if (r < 0) throw std::exception("------Error. Invalid radius.");
 		R = r;
@@ -36,9 +36,9 @@ namespace Prog2 {
 
 	double Astroid::length(double t) const
 	{
-		int k = (int) 2 * t / pi;
-		t = t - k * pi / 2;
-		return k * 1.5 * R + 1.5 * R * sin(t);
+		int k = (int)2 * t / pi;
+		t = sin(t - k * pi / 2);
+		return k * 1.5 * R + 1.5 * R * t * (t >= 0 ? t : -t);
 	}
 
 	Point Astroid::get_xy_from_t(double t) const
@@ -51,7 +51,7 @@ namespace Prog2 {
 
 	Point Astroid::f(double x) const
 	{
-		double dx = x - C.x; 
+		double dx = x - C.x;
 		dx = dx > 0 ? dx : -dx;
 		if (dx > R)
 			throw std::exception("------Error. Invalid x.");
@@ -73,23 +73,23 @@ namespace Prog2 {
 		sprintf_s(num, 20, "%.2f", C.y);
 		l += strlen(num);
 		sprintf_s(num, 20, "%.2f", R);
-		l += 2*strlen(num);
+		l += 2 * strlen(num);
 		char** s = new char* [2];
 		s[0] = new char[l];
 		s[1] = new char[l];
 		if (C.x == 0) {
-			sprintf_s(s[0], l, "x^(2/3)"); sprintf_s(s[1], l, "x = %.2f*sin(t)^3, y = ", R);
+			sprintf_s(s[0], l, "x^(2/3)"); sprintf_s(s[1], l, "x = %.2f*cos(t)^3, y = ", R);
 		}
 		else if (C.x > 0) {
-			sprintf_s(s[0], l, "(x - %.2f)^(2/3)", C.x); sprintf_s(s[1], l, "x = %.2f*sin(t)^3 + %.2f, y = ", R, C.x);
+			sprintf_s(s[0], l, "(x - %.2f)^(2/3)", C.x); sprintf_s(s[1], l, "x = %.2f*cos(t)^3 + %.2f, y = ", R, C.x);
 		}
 		else {
-			sprintf_s(s[0], l, "(x + %.2f)^(2/3)", -C.x); sprintf_s(s[1], l, "x = %.2f*sin(t)^3 - %.2f, y = ", R, -C.x);
+			sprintf_s(s[0], l, "(x + %.2f)^(2/3)", -C.x); sprintf_s(s[1], l, "x = %.2f*cos(t)^3 - %.2f, y = ", R, -C.x);
 		}
 		int k1 = strlen(s[0]);
 		int k2 = strlen(s[1]);
 		if (C.y == 0) {
-			sprintf_s(s[0] + k1, l - k1, " + y^(2/3)  = %.2f", R); sprintf_s(s[1] + k2, l - k2, "%.2f*sin(t)^3", R);
+			sprintf_s(s[0] + k1, l - k1, " + y^(2/3) = %.2f", R); sprintf_s(s[1] + k2, l - k2, "%.2f*sin(t)^3", R);
 		}
 		else if (C.y > 0) {
 			sprintf_s(s[0] + k1, l - k1, " + (y - %.2f)^(2/3) = %.2f", C.y, R); sprintf_s(s[1] + k2, l - k2, "%.2f*sin(t)^3 + %.2f", R, C.y);
