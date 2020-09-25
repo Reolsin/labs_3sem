011e20c828a387e86447cccc9ffcb7d968649c35
Reolsin <71131979+Reolsin@users.noreply.github.com>
2020-09-25 14:25:20 +0300

    Update Astroid.h

--- Astroid.h
+++ Astroid.h
@@ -22,16 +22,16 @@ namespace Prog2 {
 		Astroid(const Point& P, double r = 1);
 		Astroid(double x0, double y0, double r = 1);
 
-		Astroid& setC(const Point& P) { C = P; return *this; };
+		inline Astroid& setC(const Point& P) { C = P; return *this; };
 		Astroid& setR(double r);
 
-		Point getC() const { return C; };
-		double getR() const { return R; };
+		inline Point getC() const { return C; };
+		inline double getR() const { return R; };
 
-		double length() const { return 6 * R; } //длина всей кривой
+		inline double length() const { return 6 * R; } //длина всей кривой
 		double length(double t) const; //длину от 0 до t
-		double curvature_radius(double t) const { return (1.5 * R * sin(2 * t)); };	//радиус кривизны от t
-		double area() const { return (value * R * R); };	//площадь
+		inline double curvature_radius(double t) const { return (1.5 * R * abs(sin(2 * t))); };	//радиус кривизны от t
+		inline double area() const { return (value * R * R); };	//площадь
 		Point get_xy_from_t(double t) const;	//х и у в зависимости от t
 		Point f(double x) const;	//вернуть y в зависимости от х
 
