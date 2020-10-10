c5aa21f59514f409f9d5c8b01ecd48a0ab1c1841
Reolsin <71131979+Reolsin@users.noreply.github.com>
2020-10-10 06:24:49 +0300

    Update Stack.h

--- Lab3/Stack.h
+++ Lab3/Stack.h
@@ -2,41 +2,37 @@
 #define _STACK_H_
 #include <iostream>
 
-namespace Lab3_s {
+namespace Stack_static {
 
 	struct Cell
 	{
 		static const int len = 10;
 		double v;
 		char str[len+1];
+		Cell() : v(0), str("") {}
+		Cell(double, const char[len+1]);
+		bool string_is_correct(const char*);
+		friend std::istream& operator>>(std::istream&, Cell&);
+		friend std::ostream& operator<<(std::ostream&, const Cell&);
 	};
-
-	std::istream& operator>>(std::istream&, Cell&);
-	std::ostream& operator<<(std::ostream&, const Cell&);
 
 	class Stack
 	{
 	private:
-		static const int len = 10;
 		static const int SZ = 10;
 		int top;
 		Cell a[SZ];
 	public:
 		Stack() : top(0) {}
-		int push(const double, const char[len + 1]);
-		int push(const Cell&);
-		int pop(Cell&);
-		Stack& operator+=(const Cell& c) { this->push(c); return *this; }
-		Stack& operator()(Cell& c) { this->pop(c); return *this; }
-		Stack& copy(const Stack&);
-		Stack& move(Stack&);
-		int Status();
-		int getSZ() const { return SZ; }
-		int gettop() const { return top; }
+		Stack(Cell[], int);
+		Stack& operator+=(const Cell&);
+		Stack& operator()(Cell&);
+		inline bool not_empty() const { if (top > 0) return true; return false; }
+		inline bool not_full() const { if (top < SZ) return true; return false; }
+		inline int getSZ() const { return SZ; }
+		inline int gettop() const { return top; }
 		friend std::istream& operator>>(std::istream&, Stack&);
 		friend std::ostream& operator<<(std::ostream&, const Stack&);
 	};
-
-	bool string_is_correct(const char*, int);
 }
 #endif
