3080b3fed4de355a26d48a571606097182de621d
Reolsin <71131979+Reolsin@users.noreply.github.com>
2020-10-14 04:40:59 +0300

    Update Stack.h

--- Lab3_static/Stack.h
+++ Lab3_static/Stack.h
@@ -9,28 +9,31 @@ namespace Stack_static {
 		static const int len = 10;
 		double v;
 		char str[len+1];
-		Cell() : v(0), str("") {}
-		Cell(double, const char[len+1]);
-		bool string_is_correct(const char*);
+		Cell(double d = 0, const char s[len+1] = "");
+
 		friend std::istream& operator>>(std::istream&, Cell&);
 		friend std::ostream& operator<<(std::ostream&, const Cell&);
+
+		bool string_is_correct(const char*);
 	};
 
-	class Stack
-	{
+	class Stack {
 	private:
 		static const int SZ = 10;
 		int top;
 		Cell a[SZ];
 	public:
 		Stack() : top(0) {}
-		Stack(Cell[], int);
+		Stack(const Cell[], int);
+
 		Stack& operator+=(const Cell&);
 		Stack& operator()(Cell&);
+
 		inline bool not_empty() const { if (top > 0) return true; return false; }
 		inline bool not_full() const { if (top < SZ) return true; return false; }
 		inline int getSZ() const { return SZ; }
 		inline int gettop() const { return top; }
+
 		friend std::istream& operator>>(std::istream&, Stack&);
 		friend std::ostream& operator<<(std::ostream&, const Stack&);
 	};
