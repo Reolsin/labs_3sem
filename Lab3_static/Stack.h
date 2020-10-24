0889513343da697327aec8806b5686105d3bf090
Reolsin <71131979+Reolsin@users.noreply.github.com>
2020-10-24 18:36:21 +0300

    Update Stack.h

--- Lab3_static/Stack.h
+++ Lab3_static/Stack.h
@@ -7,13 +7,15 @@ namespace Stack_static {
 	struct Cell
 	{
 		static const int len = 10;
+		//structure data
 		double v;
 		char str[len+1];
+		//Structure constructor
 		Cell(double d = 0, const char s[len + 1] = "");
-
+		//in-out Structure overloaded operators
 		friend std::istream& operator>>(std::istream&, Cell&);
 		friend std::ostream& operator<<(std::ostream&, const Cell&);
-
+		//Function for string validation
 		bool string_is_correct(const char*);
 	};
 
@@ -23,17 +25,18 @@ namespace Stack_static {
 		int top;
 		Cell a[SZ];
 	public:
+		//constructors
 		Stack() : top(0) {}
 		Stack(const Cell[], int);
-
-		Stack& operator+=(const Cell&);
-		Stack& operator()(Cell&);
-
+		//overloaded operators
+		Stack& operator+=(const Cell&); //push
+		Stack& operator()(Cell&); //pop
+		//getters
 		inline bool not_empty() const { return top > 0; }
 		inline bool not_full() const { return top < SZ; }
 		inline int getSZ() const { return SZ; }
 		inline int gettop() const { return top; }
-
+		//in-out Stack overloaded operators
 		friend std::istream& operator>>(std::istream&, Stack&);
 		friend std::ostream& operator<<(std::ostream&, const Stack&);
 	};
