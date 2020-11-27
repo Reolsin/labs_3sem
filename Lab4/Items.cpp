e3e9487aa6edd852f399b35244060f1b8b21b2cd
Reolsin <71131979+Reolsin@users.noreply.github.com>
2020-11-27 11:29:14 +0300

    Update Items.cpp

--- Lab4/Items.cpp
+++ Lab4/Items.cpp
@@ -2,7 +2,19 @@
 
 using namespace Gamma;
 
-inline double Gamma::weapon::deal_damage()
+
+Item::Item(std::string n, int w, int up) :
+	name(n), weight(w), use_points(up) {}
+
+inline int Item::up() const { return use_points; }
+
+inline int Item::w() const { return weight; }
+
+
+weapon::weapon(std::string n, int w, int up, int f_clip_ammo, int f_ammo, int d, int rp, double a_type) :
+	Item(n, w, up), cur_clip_ammo(f_clip_ammo), full_clip_ammo(f_clip_ammo), cur_ammo(f_ammo), full_ammo(f_ammo), damage(d), reload_points(rp), ammo_type(a_type) {}
+
+inline double weapon::deal_damage()
 {
 	if (cur_clip_ammo) {
 		cur_ammo--;
@@ -27,6 +39,10 @@ bool weapon::use(Operative* unit)
 	return unit->change_weapon(this);
 }
 
+
+aidkit::aidkit(std::string n, int w, int up, int heal, int chrgs) :
+	Item(n, w, up), heal_amount(heal), charges(chrgs) {}
+
 bool aidkit::use(Operative* unit)
 {
 	if (charges) {
@@ -40,11 +56,15 @@ bool aidkit::use(Operative* unit)
 	return false;
 }
 
+
+ammunition::ammunition(std::string n, int w, int up, int count, double a_type) :
+	Item(n, w, up), cur_count(count), full_count(count), ammo_type(a_type) {}
+
 bool ammunition::use(Operative* unit)
 {
 	if (cur_count) {
 		int n;
-		if (unit->check_MP(use_points)) 
+		if (unit->check_MP(use_points))
 			if (n = unit->change_ammo(cur_count, ammo_type)) {
 				unit->change_MP(use_points);
 				cur_count -= n;
@@ -52,9 +72,4 @@ bool ammunition::use(Operative* unit)
 			}
 	}
 	return false;
-}
-
-int Gamma::Item::UP() const
-{
-	return use_points;
 }
