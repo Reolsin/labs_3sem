1a15f3829f41ffa0cc84abe518fd591626352cbf
Reolsin <71131979+Reolsin@users.noreply.github.com>
2020-11-27 11:48:48 +0300

    Update Items.h

--- Lab4/Items.h
+++ Lab4/Items.h
@@ -5,48 +5,57 @@ namespace Gamma {
 
 	class Item {
 	protected:
-		const char name;
+		std::string name;
 		int weight, use_points;
 	public:
-		Item();
-		Item(char, int, int);
-		int UP() const;
+		Item() : name("noname"), weight(), use_points() {}
+		Item(std::string, int, int);
+		~Item() {}
+
+		inline int up() const;
+		inline int w() const;
 
 		virtual bool use(Operative*) = 0;
 	};
 
+
 	class weapon : public Item {
 	private:
-		int cur_clip_ammo, cur_ammo, 
-			full_clip_ammo, full_ammo, 
+		int cur_clip_ammo, cur_ammo,
+			full_clip_ammo, full_ammo,
 			damage, reload_points;
 		double ammo_type;
 	public:
-		weapon();
-		weapon(int, int, int, int, double);
-		
+		weapon() : Item(), cur_ammo(), cur_clip_ammo(), full_ammo(), full_clip_ammo(), damage(), reload_points(), ammo_type() {}
+		weapon(std::string n, int w, int up, int f_clip_ammo, int f_ammo, int d, int rp, double a_type);
+		~weapon() {}
+
 		double deal_damage();
 		int change_ammo(int, double);
 		virtual bool use(Operative*);
 	};
 
+
 	class aidkit : public Item {
 	private:
-		int heal_amount, charges;
+		int heal_amount, charges, full_charges;
 	public:
-		aidkit();
-		aidkit(int, int);
+		aidkit() : Item(), heal_amount(), charges(), full_charges() {}
+		aidkit(std::string n, int w, int up, int heal, int chrgs);
+		~aidkit() {}
 
 		virtual bool use(Operative*);
 	};
+
 
 	class ammunition : public Item {
 	private:
 		int cur_count, full_count;
 		double ammo_type;
 	public:
-		ammunition();
-		ammunition(int, double);
+		ammunition() : Item(), cur_count(), full_count(), ammo_type() {}
+		ammunition(std::string n, int w, int up, int count, double a_type);
+		~ammunition() {}
 
 		virtual bool use(Operative*);
 	};
