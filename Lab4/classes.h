1f03c0c8872044efb9ef42d7a2f01c59cf100f45
Reolsin <71131979+Reolsin@users.noreply.github.com>
2020-11-27 11:50:30 +0300

    Create classes.h

---
+++ Lab4/classes.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <map>
+#include <fstream>
+#include "Units.h"
+#include "Items.h"
+#include "Map.h"
+
+
+namespace Gamma {
+
+	class GameField;
+	class Cell;
+
+	class Unit;
+	class Operative;
+	class Alien_melee;
+	class Alien_range;
+	class Alien_friendly;
+
+	class Item;
+	class aidkit;
+	class ammunition;
+	class weapon;
+}
