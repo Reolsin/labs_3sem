c4cd0d52f4ff7bfeb5704e50b475c2d86017c346
Reolsin <71131979+Reolsin@users.noreply.github.com>
2020-11-27 11:49:56 +0300

    Update Units.h

--- Lab4/Units.h
+++ Lab4/Units.h
@@ -8,30 +8,33 @@ namespace Gamma {
 		int weight, num;
 		std::map<char, Item*> Items;
 	public:
-		Backpack();
-		Backpack(int, const char*, int);
+		Backpack(std::vector<char> binds = { 'q','w','e','r' });
+		~Backpack() {}
 
 		bool put_item(Item*);
 		Item* drop_item(char);
 		void delete_item(char);
-		inline Item* choose_item(char bind);
+		inline Item* choose_item(char bind) const;
 	};
 
 
 	class Unit {
 	private:
-		const char name;
+		std::string name;
 		int cur_HP, cur_MP;
 		const int full_HP, full_MP,
-			vision_Rad,
+			vision,
 			move_points;
 		int x, y;
 	public:
-		Unit();
+		Unit() : name("noname"), x(), y(), cur_HP(), full_HP(), cur_MP(), full_MP(), vision(), move_points() {}
+		Unit(std::string n, int x_ = -1, int y_ = -1, int hp = 100, int mp = 100, int r = 5, int move = 20);
+		~Unit() {}
+
 		int change_HP(int);
 		inline void change_MP(int);
 		inline bool check_MP(int) const;
-		inline bool is_Alive() const;
+		inline bool is_alive() const;
 	};
 
 
@@ -42,6 +45,11 @@ namespace Gamma {
 		Backpack backpack;
 		weapon* gun;
 	public:
+		Operative() : Unit(), backpack(), gun(nullptr), accuracy(), full_weight() {}
+		Operative(std::vector<char> binds, weapon* g, double ac, int f_w, 
+			std::string n, int x = -1, int y = -1, int hp = 100, int mp = 100, int r = 5, int move = 20);
+		~Operative() { delete gun; }
+
 		bool attack(Unit*);
 
 		bool use_item(char bind);
@@ -56,6 +64,11 @@ namespace Gamma {
 		int damage;
 		int attack_points;
 	public:
+		Alien_melee() : Unit(), accuracy(), damage(), attack_points() {}
+		Alien_melee(double ac, int d, int ap, 
+			std::string n, int x = -1, int y = -1, int hp = 100, int mp = 100, int r = 5, int move = 20);
+		~Alien_melee() {}
+
 		bool attack(Unit*);
 	};
 
@@ -65,6 +78,10 @@ namespace Gamma {
 		double accuracy;
 		weapon* gun;
 	public:
+		Alien_range() : Unit(), accuracy(), gun(nullptr) {}
+		Alien_range(weapon* g, double ac, std::string n, int x = -1, int y = -1, int hp = 100, int mp = 100, int r = 5, int move = 20);
+		~Alien_range() { delete gun; }
+
 		bool attack(Unit*);
 	};
 
@@ -73,5 +90,9 @@ namespace Gamma {
 	private:
 		Backpack backpack;
 	public:
+		Alien_friendly() : Unit(), backpack() {}
+		Alien_friendly(std::string n, int x = -1, int y = -1, int hp = 100, int mp = 100, int r = 5, int move = 20);
+		~Alien_friendly() {}
+
 	};
 }
