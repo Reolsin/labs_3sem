f6e9a1a93c67d54bb45b396c1f69c361f0f0f999
Reolsin <71131979+Reolsin@users.noreply.github.com>
2020-12-29 18:25:11 +0300

    Update classes.h

--- Lab4/classes.h
+++ Lab4/classes.h
@@ -24,4 +24,12 @@ namespace Gamma {
 	class aidkit;
 	class ammunition;
 	class weapon;
+
+	const std::string msgs[] = {
+		"Success.", "Need to reload weapon.", "Unit doesnt have enough action points.", "Unit has full HP.", "Aidkit hasnt charges.",
+		"Weapon has full clip or wrong ammo type.", "No ammo in container.", "Choosed empty slot.", "Unit cant move here.", "Unit is dead.",
+		"Inventory mode activated.", "Unit dont have Backpack", "Choose item to take.", "No items on this cell.", "Unit cant take this item.", 
+		"No dead units on this cell.", "Choose target.", "Unit dont have weapon." };
+
+	const std::string close_mods[] = { "Game loaded.", "Inventory mode closed.", "Attack mode closed.", "Take mode closed.", "Loot mode closed." };
 }
