54cbee39661b24d496b53ecb1fc6ba6ee5de5de0
Reolsin <71131979+Reolsin@users.noreply.github.com>
2020-12-29 18:28:48 +0300

    Create Menu.cpp

---
+++ Lab4/Menu.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include "conio.h"
+
+
+class Menu {
+	std::string pointer;
+	std::vector<std::string> messages;
+	void show(int pos)
+	{
+		for (int i = 0, n = (60 - messages.size()) / 2; i < n; i++)
+			std::cout << std::endl;
+
+		for (size_t i = 0; i < messages.size(); i++) {
+			std::cout << messages[i];
+			if (pos == i)
+				std::cout << pointer;
+			std::cout << std::endl;
+		}
+	}
+
+public:
+	Menu(std::vector<std::string> msgs, std::string p = "<") : \
+		messages(msgs), pointer(p) {}
+
+	int get_recall()
+	{
+		int rc = 0;
+		int switcher;
+		system("cls");
+		do {
+			show(rc);
+			switcher = _getch();
+			if (switcher == 224)
+				switcher = _getch();
+			switch (switcher) {
+			case 72:
+				rc = (messages.size() + rc - 1) % messages.size();
+				break;
+			case 80:
+				rc = (messages.size() + rc + 1) % messages.size();
+				break;
+			}
+			system("cls");
+		} while (switcher != 13);
+
+		return rc;
+	}
+
+	inline bool is_last(int n) { return n == messages.size() - 1; }
+};
