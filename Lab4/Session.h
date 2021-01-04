567c3484847e6f9939ef43ac0047b22fc88f0a33
Reolsin <71131979+Reolsin@users.noreply.github.com>
2021-01-04 14:13:29 +0300

    Update Session.h

--- Lab4/Session.h
+++ Lab4/Session.h
@@ -1,52 +1,62 @@
 #pragma once
-#include "classes.h"
+#include "Declarations.h"
 #include "Units.h"
 #include "Items.h"
+#include <iostream>
+
 
 namespace Gamma {
 
+	/// A GameLevel structure that stores the current cell_type and items on it.
 	struct Cell {
-		std::vector<Item*> Items;
-		char cell_type;
+		My_vector<Item*> Items;
+		char cell_type; ///< Char that displays on interface.
 
 		Cell() : cell_type('.') {}
 		~Cell();
 
-		bool add_item(Item*);
+		bool add_item(Item*); ///< Method for adding items on Cell.
 	};
 
+	///The class responsible for storing the map and game items on it.
 	class GameLevel {
-		std::vector<std::vector<Cell>> Map;
-		int h, l;
+	private:
+		My_vector<My_vector<Cell>> Map;
 	protected:
 		std::ofstream& save(std::ofstream&) const;
 	public:
 		GameLevel(int l_ = 100, int h_ = 100);
 		GameLevel(std::ifstream&);
 
-		std::vector<Item*>* get_items(const Point& p);
+		My_vector<Item*>* get_items(const Point& p);
 		bool add_item(Item*, int, int);
 		Cell& operator[](const Point&);
+		const Cell& operator[](const Point&) const;
 
 		bool point_access(int, int) const;
 		bool point_on_map(double, double) const;
 		bool is_transparent(double, double) const;
-		std::vector<std::vector<char>> render_vision(const Unit*, const GameSession*) const;
 
 		friend std::ofstream& operator<<(std::ofstream& ofile, const GameLevel& level) { return level.save(ofile); }
 	};
 
+	/// Enum that declares all possible interface modes.
 	enum class mode { game, inventory, attack, take, loot };
+	/// Structure for processing user commands in different conditions.
 	struct operation_code {
 		int code;
 		mode m;
 	};
+	/// Comparison functor for structure operation_code.
 	struct CompareType {
 		using is_transparent = std::true_type;
-		bool operator()(const operation_code& lhs, const operation_code& rhs) const 
-		{ return (lhs.code < rhs.code) || ((lhs.code == rhs.code) && (lhs.m < rhs.m)); }
+		bool operator()(const operation_code& lhs, const operation_code& rhs) const
+		{
+			return (lhs.code < rhs.code) || ((lhs.code == rhs.code) && (lhs.m < rhs.m));
+		}
 	};
 
+	/// The main class responsible for handling user actions and rendering the interface and map.
 	class GameSession {
 	private:
 		static std::map<operation_code, bool (Gamma::GameSession::*)(), CompareType> operation;
@@ -55,15 +65,16 @@ namespace Gamma {
 
 		GameLevel level;
 
-		Operative* current_unit;
+		Unit* current_unit;
 		int current_unit_num;
 		Point attack_cursor;
-		std::vector<std::vector<char>> unit_vision;
-		std::vector<Item*>* displayed_items;
-		int displayed_items_num;
+		My_vector<My_vector<char>> unit_vision;
+		My_vector<Item*>* displayed_items;
+		My_vector<Item**> lootmode_items;
+		size_t disp_items_num;
 
-		std::vector<Operative*> player;
-		std::vector<Unit*> enemy;
+		My_vector<Unit*> player;
+		My_vector<Unit*> enemy;
 
 		mode current_mode;
 
@@ -84,6 +95,8 @@ namespace Gamma {
 		bool loot_activate();
 		bool table_choose_up();
 		bool table_choose_down();
+		bool loot_choose_up();
+		bool loot_choose_down();
 
 		void attack_cursor_move(const Point&);
 		bool attack_init();
@@ -98,12 +111,16 @@ namespace Gamma {
 		bool exit();
 
 	public:
-		GameSession() : con_len(160), message(close_mods), current_unit_num(0), current_unit(nullptr), displayed_items_num(0), displayed_items(nullptr), attack_cursor(), current_mode(mode::game), level() {}
-		GameSession(std::ifstream&);
+		GameSession() : con_len(160), message(close_mods), current_unit_num(0), current_unit(nullptr), disp_items_num(0), displayed_items(nullptr), attack_cursor(), current_mode(mode::game), level() {}
+		GameSession(std::ifstream&); ///< Constructor that builds object with loading config file.
 		~GameSession();
 
-		Unit* unit_here(const Point&) const;
-		bool current_unit_is_alive() const { return current_unit->is_alive(); }
+		Unit* alive_unit_here(const Point&) const;
+		///< A method for detecting a living unit on a map slot.
+		My_vector<Unit*> dead_units_here(const Point& p) const;
+		///< A method for detecting a dead units on a map slot. \details May return several Unit s.
+		My_vector<My_vector<char>> render_vision(const Unit* unit) const;
+		///< A method for detecting a living unit on a map slot.
 
 		bool execute(int button)
 		{
@@ -111,10 +128,12 @@ namespace Gamma {
 				return (this->*operation.at({ button, current_mode }))();
 			return true;
 		}
+		///< A method is responsible for processing user keystrokes. \details Method build operation_code structure and calls methods from private area of visibility.
 		bool check() const;
-
+		///< A method for checking if all units from any team are dead.\details For ending the current GameSession.
 		void draw() const;
-
+		///< A method for drawing all interface.
 		bool save(std::ofstream&);
+		///< A method for saving current session in format of loading config file.
 	};
 }
