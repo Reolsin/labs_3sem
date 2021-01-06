f5f36b66fe4da8289276cf63c7e0658c61d626f9
Reolsin <71131979+Reolsin@users.noreply.github.com>
2021-01-06 13:19:48 +0300

    Update Menu.cpp

--- Lab4/Menu.cpp
+++ Lab4/Menu.cpp
@@ -1,53 +1,36 @@
-#include <iostream>
-#include "../my_vector/My_vector.h"
-#include "conio.h"
+#include "Menu.h"
 
-
-using My_vect::My_vector;
-
-class Menu {
-	std::string pointer;
-	My_vector<std::string> messages;
-	void show(int pos)
-	{
-		for (int i = 0, n = (60 - messages.size()) / 2; i < n; i++)
-			std::cout << std::endl;
-
+void Menu::show(int pos)
+{
+	for (int i = 0, n = (60 - messages.size()) / 2; i < n; i++)
+		std::cout << std::endl;
 		for (size_t i = 0; i < messages.size(); i++) {
-			std::cout << messages[i];
-			if (pos == i)
-				std::cout << pointer;
-			std::cout << std::endl;
-		}
+		std::cout << messages[i];
+		if (pos == i)
+			std::cout << pointer;
+		std::cout << std::endl;
 	}
+}
 
-public:
-	Menu(My_vector<std::string> msgs, std::string p = "<") : \
-		messages(msgs), pointer(p) {}
-
-	int get_recall()
-	{
-		int rc = 0;
-		int switcher;
-		system("cls");
-		do {
-			show(rc);
+int Menu::get_recall()
+{
+	int rc = 0;
+	int switcher;
+	system("cls");
+	do {
+		show(rc);
+		switcher = _getch();
+		if (switcher == 224)
 			switcher = _getch();
-			if (switcher == 224)
-				switcher = _getch();
-			switch (switcher) {
-			case 72:
-				rc = (messages.size() + rc - 1) % messages.size();
-				break;
-			case 80:
-				rc = (messages.size() + rc + 1) % messages.size();
-				break;
-			}
-			system("cls");
-		} while (switcher != 13);
-
+		switch (switcher) {
+		case 72:
+			rc = (messages.size() + rc - 1) % messages.size();
+			break;
+		case 80:
+			rc = (messages.size() + rc + 1) % messages.size();
+			break;
+		}
+		system("cls");
+	} while (switcher != 13);
 		return rc;
-	}
-
-	inline bool is_last(int n) { return n == messages.size() - 1; }
-};
+}
