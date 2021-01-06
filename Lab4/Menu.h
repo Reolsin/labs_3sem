430b86e2b4d202a8bff7f83ab216c9614c3b2eed
Reolsin <71131979+Reolsin@users.noreply.github.com>
2021-01-06 13:17:49 +0300

    Update Menu.h

--- Lab4/Menu.h
+++ Lab4/Menu.h
@@ -1,1 +1,20 @@
+#include <iostream>
+#include "../my_vector/My_vector.h"
+#include "conio.h"
 
+
+using My_vect::My_vector;
+
+class Menu {
+	std::string pointer;
+	My_vector<std::string> messages;
+	void show(int pos);
+
+public:
+	Menu(My_vector<std::string> msgs, std::string p = "<") : \
+		messages(msgs), pointer(p) {}
+
+	int get_recall();
+
+	inline bool is_last(int n) { return n == messages.size() - 1; }
+};
